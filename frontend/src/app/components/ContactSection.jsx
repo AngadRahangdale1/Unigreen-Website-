3c26794d4d715de5254e38c7ea6cef7e34f18fa2
Angad Rahangdale <angadkumaar82@gmail.com>
2026-06-30 15:55:47 +0530

    fix: update EmailJS template ID

--- frontend/src/app/components/ContactSection.jsx
+++ frontend/src/app/components/ContactSection.jsx
@@ -41,7 +41,7 @@ export default function ContactSection() {
 
       const result = await emailjs.send(
         "service_2c11aty",
-        "lhd18v9",
+        "template_2rkdze9",
         templateParams,
         "onTxtGmFd8P0xyElT"
       );
